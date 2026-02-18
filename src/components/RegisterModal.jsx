e761b2c5a8dc5cb70fb89838dcc76dea62952732
Jeremiah Isaiah <55800485+JIsaiah@users.noreply.github.com>
2026-02-18 16:00:16 +0900

    アカウント作成ページの同意するボタンも真ん中にした

--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -400,7 +400,7 @@ export function RegisterModal({ onRegister, onBackToLogin }) {
             style={{
               display: "flex",
               alignItems: "center",
-              justifyContent: "flex-end",
+              justifyContent: "center",
               gap: "8px",
             }}
           >
